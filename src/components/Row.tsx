0ddc5bf49350422ca773d258c605d3bb334bcd0c
nargok <nargok@gmail.com>
2021-05-02 13:24:58 +0900

    add youtube trailer

--- src/components/Row.tsx
+++ src/components/Row.tsx
@@ -1,4 +1,5 @@
 import { useState, useEffect } from 'react'
+import Youtube from 'react-youtube'
 import axios from './../axios'
 import './Row.scss'
 
@@ -19,8 +20,17 @@ type Movie = {
   backdrop_path: string
 }
 
+type Options = {
+  height: string
+  width: string
+  playerVars: {
+    autoplay: 0 | 1 | undefined
+  }
+}
+
 export const Row = ({ title, fetchUrl, isLargeRow }: Props) => {
   const [movies, setMovies] = useState<Movie[]>([])
+  const [trailerUrl, setTrailerUrl] = useState<string | null>('')
 
   useEffect(() => {
     async function fetchData() {
@@ -30,6 +40,25 @@ export const Row = ({ title, fetchUrl, isLargeRow }: Props) => {
     }
     fetchData()
   }, [fetchUrl])
+
+  const opts: Options = {
+    height: '390',
+    width: '640',
+    playerVars: {
+      autoplay: 1,
+    },
+  }
+
+  const handleClick = async (movie: Movie) => {
+    if (trailerUrl) {
+      setTrailerUrl('')
+    } else {
+      let trailerUrl = await axios.get(
+        `movie/${movie.id}/videos?api_key=${process.env.REACT_APP_API_KEY}`,
+      )
+      setTrailerUrl(trailerUrl.data.results[0]?.key)
+    }
+  }
 
   return (
     <div className="Row">
@@ -43,9 +72,11 @@ export const Row = ({ title, fetchUrl, isLargeRow }: Props) => {
               isLargeRow ? movie.poster_path : movie.backdrop_path
             }`}
             alt={movie.name}
+            onClick={() => handleClick(movie)}
           />
         ))}
       </div>
+      {trailerUrl && <Youtube videoId={trailerUrl} opts={opts} />}
     </div>
   )
 }
