93b3c69873e273fd9e5b0b98b220251e42da2add
nargok <nargok@gmail.com>
2021-05-02 12:28:46 +0900

    nav component

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,12 @@
 import { Row } from 'components/Row'
 import { Banner } from 'components/Banner'
+import { Nav } from 'components/Nav'
 import { requests } from './request'
 
 function App() {
   return (
     <div className="App">
+      <Nav />
       <Banner />
       <Row
         title="NETFLIX CLONE"
